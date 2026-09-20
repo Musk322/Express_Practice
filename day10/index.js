c6d0cd1092d181da79e5ae8e9058f12cdb1c038d
Musk322 <muski7806@gmail.com>
2026-09-20 23:01:14 +0530

    code updated

---
+++ day10/index.js
@@ -0,0 +1,34 @@
+// Application-Level middelware:
+
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+const loggerMiddleware = (req,res,next)=>{
+    console.log(`[${new Date().toLocaleDateString()}]) ${req.method} $ {req.url}`);
+    next();
+}
+app.use(loggerMiddleware);
+
+app.use('/admin',(req,res,next)=>{
+    console.log('Admin section Accessed');
+    next();
+});
+
+app.get('/',(req,res)=>{
+    res.send('Hello Home!');
+});
+
+app.get('/about',(req,res)=>{
+    res.send('Hello About Page!');
+});
+
+app.get('/admin/dashboard',(req,res)=>{
+    res.send('Hello Admin Page!');
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
