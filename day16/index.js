df0da7eed1a54e01202f00a2adae5df2e596a245
Musk322 <muski7806@gmail.com>
2026-09-24 23:03:30 +0530

    code updated

---
+++ day16/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+app.get("/",(req,res)=>{
+    res.send("Hello Page!");
+});
+
+app.get('/fail',(req,res,next)=>{
+    const err = new Error("Somenthing went wrong!");
+    err.statusCode = 400;
+    next(err);
+});
+
+//ERROR HANDLING MIDDELWARE
+
+app.use((err,req,res,next)=>{
+    console.log("Error caught by middelware:",err.message);
+    res.status(err.statusCode||500).JSON({
+        sucess:false,
+        message:err.message||'Internal Server Error'
+    });
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
